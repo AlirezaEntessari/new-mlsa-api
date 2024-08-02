dcf04c8ac34e59115674e10d933e6f75c2c3505b
Alireza Entessari <alintssr@gmail.com>
2024-08-02 18:04:07 -0400

    Added backend

--- db.sql
+++ db.sql
@@ -1,6 +1,12 @@
 CREATE DATABASE newmlsa;
 
-CREATE TABLE user_information (
+CREATE TABLE agency_information (
     "Email" VARCHAR(255),
-    "Membership Plan" VARCHAR(255)
+    "Membership Plan" VARCHAR(255),
+    "Staffing Agency Name" VARCHAR(255),
+    "Staffing Agency EIN" VARCHAR(255),
+    "Staffing Agency Website" VARCHAR(255),
+    "Industry Field" VARCHAR(255),
+    "Full Name (Admin)" VARCHAR(255),
+    "Password" VARCHAR(255)
 );
--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
+const app = express();
+
 app.use(cors());
-app.use(express.json()); // Add this line to parse JSON request bodies
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route to handle signup
-app.post("/api/signup", async (req, res) => {
-  try {
-    const { email } = req.body;
-    const newUser = await pool.query(
-      'INSERT INTO user_information ("Email") VALUES ($1) RETURNING *',
-      [email]
-    );
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-// Route to handle membership plan selection
-app.put("/api/select-plan", async (req, res) => {
+// Route to handle email submission
+app.post("/api/agency_information", async (req, res) => {
   try {
     const { email, membershipPlan } = req.body;
-    const updateUser = await pool.query(
-      'UPDATE user_information SET "Membership Plan" = $1 WHERE "Email" = $2 RETURNING *',
-      [membershipPlan, email]
-    );
-    res.json(updateUser.rows[0]);
+    if (email) {
+      const newEmailEntry = await pool.query(
+        'INSERT INTO agency_information ("Email") VALUES ($1) RETURNING *',
+        [email]
+      );
+      res.json(newEmailEntry.rows[0]);
+    } else if (membershipPlan) {
+      const updatePlan = await pool.query(
+        'UPDATE agency_information SET "Membership Plan" = $1 WHERE "Email" IS NOT NULL RETURNING *',
+        [membershipPlan]
+      );
+      res.json(updatePlan.rows[0]);
+    } else {
+      res.status(400).send("Invalid request");
+    }
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send("Server error");
   }
 });
 
