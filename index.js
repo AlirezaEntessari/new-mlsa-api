9c01bd6963e9a575f8e6d989b810d6f1924f44e0
Alireza Entessari <alintssr@gmail.com>
2025-01-07 18:18:34 -0500

    Added route for posted candidates

--- index.js
+++ index.js
@@ -522,7 +522,7 @@ app.post("/post-candidate", async (req, res) => {
       "Experience Level", "Salary Type", "Environment", "Start Date",
       "Willing to Travel", "Willing to Relocate", "Education", "LinkedIn",
       "Facebook", "Twitter (X)", "Portfolio", "Candidate Description/Notes", "Skills"
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28)
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29)
   `;
 
   try {
